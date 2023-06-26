59bc0345dd44e7597b4132ad5632192d084c1ffd
Lay Yuu <laypyay.this@gmail.com>
2023-06-26 01:11:31 -0700

    update news and Markdown

--- codes/news.js
+++ codes/news.js
@@ -1,1 +1,6 @@
 console.log("This is BBC")
+
+fetch('https://restcountries.com/v3.1/all')
+    .then((res) => { res.json() })
+    .then((data) => { console.log(data) })
+    .catch((err) => { console.log(err) })
